5441309af8430eb914b190aa7ba1c140bc9ce858
rohit.bagdi <rohit.bagdi@cmss.in>
2021-11-17 23:00:24 +0530

    first commit

---
+++ src/main/java/com/infosys/irs/InfyGoBootLoginApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.irs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfyGoBootLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyGoBootLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infosys/irs/InfyGoBootLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.irs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyGoBootLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
